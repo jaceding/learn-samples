555dba2b6a88c05305fe49eb54096148a4a49047
jaceding <931977674@qq.com>
2021-06-07 15:16:51 +0800

    feat: 测试Decimal

---
+++ demo-java/src/main/java/per/jaceding/demo/decimal/BigDecimalTest.java
@@ -0,0 +1,20 @@
+package per.jaceding.demo.decimal;
+
+import java.math.BigDecimal;
+
+/**
+ * @author jaceding
+ * @date 2021/6/7
+ */
+public class BigDecimalTest {
+
+    @SuppressWarnings("AlibabaBigDecimalAvoidDoubleConstructor")
+    public static void main(String[] args) {
+        BigDecimal a = new BigDecimal(1.01);
+        BigDecimal b = new BigDecimal(1.02);
+        BigDecimal c = new BigDecimal("1.01");
+        BigDecimal d = new BigDecimal("1.02");
+        System.out.println(a.add(b));
+        System.out.println(c.add(d));
+    }
+}
